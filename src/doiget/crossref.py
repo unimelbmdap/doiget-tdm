2c7fa2bd6f2cdd144732544ad838464ef864e45f
Damien Mannion <damien@djmannion.net>
2024-09-27 10:18:04 +1000

    -

--- src/doiget/crossref.py
+++ src/doiget/crossref.py
@@ -9,17 +9,26 @@ import doiget.web
 LOGGER = logging.getLogger(__name__)
 LOGGER.addHandler(logging.NullHandler())
 
-POLITE_POOL_WARNED = False
 
 class CrossRefWebAPI:
 
     def __init__(self) -> None:
+
+        self.polite_pool_warned = False
 
         self.base_url = "https://api.crossref.org/"
 
         self._session = doiget.web.WebRequester(
             headers={"User-Agent": self.user_agent},
         )
+
+        self.default_limit = pyrate_limiter.Limiter(
+            pyrate_limiter.RequestRate(
+                limit=50,
+                interval=1
+            )
+        )
+
 
     @property
     def user_agent(self) -> str:
@@ -29,12 +38,11 @@ class CrossRefWebAPI:
         if (email := doiget.config.SETTINGS.email_address) is not None:
             lib_name += f"; mailto:{email}"
         else:
-            global POLITE_POOL_WARNED
-            if not POLITE_POOL_WARNED:
+            if not self.polite_pool_warned:
                 LOGGER.warning(
                     "Email address not configured; unable to use CrossRef polite pool"
                 )
-                POLITE_POOL_WARNED = True
+                self.polite_pool_warned = True
 
         lib_name += ")"
 
@@ -44,29 +52,22 @@ class CrossRefWebAPI:
 
         return lib_name
 
-    def get_rate_limit(self, test_url: str) -> pyrate_limiter.Limiter:
-
-        default_limit = pyrate_limiter.Limiter(
-            pyrate_limiter.RequestRate(
-                limit=50,
-                interval=1
-            )
-        )
+    def get_rate_limit(self) -> pyrate_limiter.Limiter:
 
         default_msg = "Rate limit could not be identified; using defaults"
 
         try:
-            response = self._session.get(url=test_url)
+            response = self._session.get(url=self.base_url)
         except Exception:
             LOGGER.warning(default_msg)
-            return default_limit
+            return self.default_limit
 
         if (
             "x-ratelimit-limit" not in response.headers
             or "x-ratelimit-interval" not in response.headers
         ):
             LOGGER.warning(default_msg)
-            return default_limit
+            return self.default_limit
 
         n_calls = int(response.headers["x-ratelimit-limit"])
         period_s = int(response.headers["x-ratelimit-interval"][:-1])
@@ -83,3 +84,16 @@ class CrossRefWebAPI:
         )
 
         return limit
+
+    def call(
+        self,
+        query: str,
+    ) -> requests.Response:
+
+        url = f"{self.base_url}{query}"
+
+        response = self._session.get(url=url)
+
+        response.raise_for_status()
+
+        return response
---
+++ tests/test_crossref.py
@@ -0,0 +1,111 @@
+
+import time
+
+import pytest
+
+import requests
+import requests.exceptions
+import tenacity
+import requests_ratelimiter
+import pyrate_limiter
+
+import doiget.config
+import doiget.crossref
+
+
+def test_rate_limit(monkeypatch) -> None:
+
+    cr = doiget.crossref.CrossRefWebAPI()
+
+    def mock_get_raise(*args, **kwargs) -> None:
+        raise requests.exceptions.RequestException()
+
+    monkeypatch.setattr(cr._session._session, "get", mock_get_raise)
+
+    rate_limit = cr.get_rate_limit()
+
+    (rates,) = rate_limit._rates
+    (default_rates,) = cr.default_limit._rates
+
+    assert rates.interval == default_rates.interval
+    assert rates.limit == default_rates.limit
+
+    mock_limit = 10
+    mock_period = 30
+
+    def mock_get_headers(*args, **kwargs) -> None:
+        response = requests.Response()
+        response.headers["x-ratelimit-limit"] = str(mock_limit)
+        response.headers["x-ratelimit-interval"] = f"{mock_period}s"
+        return response
+
+    monkeypatch.setattr(cr._session._session, "get", mock_get_headers)
+
+    rate_limit = cr.get_rate_limit()
+
+    (rates,) = rate_limit._rates
+
+    assert rates.interval == mock_period
+    assert rates.limit == mock_limit
+
+    def mock_get_headers_empty(*args, **kwargs) -> None:
+        response = requests.Response()
+        return response
+
+    monkeypatch.setattr(cr._session._session, "get", mock_get_headers_empty)
+
+    rate_limit = cr.get_rate_limit()
+
+    (rates,) = rate_limit._rates
+
+    assert rates.interval == default_rates.interval
+    assert rates.limit == default_rates.limit
+
+
+def test_call(monkeypatch) -> None:
+
+    cr = doiget.crossref.CrossRefWebAPI()
+
+    def mock_get_raise(*args, **kwargs) -> None:
+        raise requests.exceptions.RequestException()
+
+    monkeypatch.setattr(cr._session._session, "get", mock_get_raise)
+
+    with pytest.raises(requests.exceptions.RequestException):
+        cr.call(query="")
+
+
+    def mock_get_raise_specific(*args, **kwargs) -> None:
+        response = requests.Response()
+        response.status_code = 404
+        return response
+
+    monkeypatch.setattr(cr._session._session, "get", mock_get_raise_specific)
+
+    with pytest.raises(requests.exceptions.HTTPError):
+        cr.call(query="")
+
+    def mock_get_ok(*args, **kwargs) -> None:
+        response = requests.Response()
+        response.status_code = 200
+        return response
+
+    monkeypatch.setattr(cr._session._session, "get", mock_get_ok)
+
+    cr.call(query="")
+
+
+def test_user_agent():
+
+    mock_email = "test@test.com"
+
+    cr = doiget.crossref.CrossRefWebAPI()
+
+    doiget.config.SETTINGS.email_address = None
+
+    assert mock_email not in cr.user_agent
+
+    doiget.config.SETTINGS.email_address = mock_email
+
+    assert f"; mailto:{mock_email}" in cr.user_agent
+
