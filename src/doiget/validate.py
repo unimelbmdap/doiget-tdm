8970b799de6c426cf4c8042fa5678c6cd1763755
Damien Mannion <damien.mannion@unimelb.edu.au>
2024-09-30 15:06:33 +1000

    -

--- src/doiget/validate.py
+++ src/doiget/validate.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+
+
+import puremagic
 
 import doiget.format
 
@@ -7,10 +11,28 @@ def validate_data(
     data: bytes,
 ) -> None:
 
-    pass
+    validators = {
+        doiget.format.FormatName.XML: validate_xml,
+        doiget.format.FormatName.PDF: validate_pdf,
+        doiget.format.FormatName.HTML: validate_html,
+        doiget.format.FormatName.TXT: validate_txt,
+        doiget.format.FormatName.TIFF: validate_tiff,
+    }
+
+    validator_func = validators[data_format]
+
+    validator_func(data=data)
 
 
 def validate_xml(data: bytes) -> None:
+    pass
+
+
+def validate_html(data: bytes) -> None:
+    pass
+
+
+def validate_txt(data: bytes) -> None:
     pass
 
 
@@ -20,4 +42,13 @@ def validate_pdf(data: bytes) -> None:
 
     if extension != ".pdf":
         msg = f"Expected PDF, but inferred type {extension}"
+        raise ValueError(msg)
+
+
+def validate_tiff(data: bytes) -> None:
+
+    extension = puremagic.from_string(string=data)
+
+    if extension != ".tiff":
+        msg = f"Expected TIFF, but inferred type {extension}"
         raise ValueError(msg)
